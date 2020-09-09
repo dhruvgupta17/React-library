0e9534c071a6ec76ead9af7b8c58234ce6a25229
dhruvg <dhruvg>
2020-09-09 23:09:59 +0530

    my first code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import ReactDOM from 'react-dom';
+import Img from 'react-optimized-image';
+import Logo from './logo.png';
+import Sidemenu from './Dashboard/sidemenu';
+import 'bootstrap/dist/css/bootstrap.css';
+function Posts(){
+    const box = [
+       {
+        id:1,
+        name :"web demo",
+        img:"https://s3-ap-south-1.amazonaws.com/sleepycat-wordpress-test/wp-content/uploads/2020/05/28132254/animated-logo-new3.gif"
+      },  
+      {
+        id:2,
+        name :"faceBook",
+        img:"https://static.xx.fbcdn.net/rsrc.php/y8/r/dF5SId3UHWd.svg"
+      } ,
+      ]
+   
+    const listitem = box.map((post)=> 
+    <div>
+      <h1>{post.id}</h1>
+      <p>{post.name}</p>
+      <img src={post.img}/>
     </div>
-  );
-}
+    )
+    return(
+      <div>
+        {listitem}
+      </div>
+    ) 
+  }
 
-export default App;
+
+class App extends React.Component { 
+ render() {
+  return(
+   <div id="mount">
+   <Sidemenu/>
+   {/* <Posts/> */}
+   </div>
+  );
+ }
+}
+export default App
